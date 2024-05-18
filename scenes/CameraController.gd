a51444ef53a5e1dfea98c71edf9ae09c454012d2
Matthew-Riddle <matthew.b.riddle@gmail.com>
2024-05-18 13:50:35 -0500

    added camera toggle, organized files into folders

---
+++ scenes/CameraController.gd
@@ -0,0 +1,21 @@
+extends Node3D
+@onready var fps_camera = get_tree().get_root().get_node("TestMap/Player/PlayerSpatial/Camera3D")
+@onready var rts_camera = get_tree().get_root().get_node("TestMap/CameraBase/CameraSocket/Camera3D")
+@onready var camera_toggle = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rts_camera.make_current()
+	pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_camera_toggle"):
+		if camera_toggle == false:
+			fps_camera.make_current()
+		else:
+			rts_camera.make_current()
+		camera_toggle = !camera_toggle
+	#if Input.is_action_just_pressed("ui_camera_toggle") and fps_camera.is_current() == true:
+		#rts_camera.make_current()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
